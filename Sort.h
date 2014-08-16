1b515f2e94298ce0ae3843c11fa2dd5442a86f2a
Du Zixi <duzixi@gmail.com>
2014-08-16 23:15:50 +0800

    Create Sort.h

---
+++ Sort.h
@@ -0,0 +1,16 @@
+//
+//  Sort.h
+//  SortGame
+//
+//  Created by 杜子兮(duzix) on 14-8-16.
+//  Copyright (c) 2014年 lanou3g.com All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sort : NSObject
+
+///  生成随机数组
++ (NSMutableArray *) createRandomArray;
+
+@end
