d4ea18eb8dddd2d6f6d7ee339dc4b9cc5a28c963
Du Zixi <duzixi@gmail.com>
2014-08-28 02:05:10 +0800

    Update BubbleSortViewController.h

--- Source/BubbleSortViewController.h
+++ Source/BubbleSortViewController.h
@@ -3,8 +3,8 @@
 //  冒泡排序
 //  SortGame
 //
-//  Created by 杜子兮(duzix) on 14-8-2.
-//  Copyright (c) 2014年 lanou. All rights reserved.
+//  Created by 杜子兮(duzixi) on 14-8-16.
+//  Copyright (c) 2014年 lanou3g.com 蓝鸥科技 All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
