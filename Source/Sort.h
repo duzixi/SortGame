942f78b11657302538fd166b7b2f5e9be9fcc29c
Du Zixi <duzixi@gmail.com>
2014-08-28 02:09:12 +0800

    Update Sort.h

--- Source/Sort.h
+++ Source/Sort.h
@@ -2,8 +2,8 @@
 //  Sort.h
 //  SortGame
 //
-//  Created by 杜子兮(duzix) on 14-8-16.
-//  Copyright (c) 2014年 lanou3g.com All rights reserved.
+//  Created by 杜子兮(duzixi) on 14-8-16.
+//  Copyright (c) 2014年 lanou3g.com 蓝鸥科技 All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -12,5 +12,8 @@
 
 ///  生成随机数组
 + (NSMutableArray *) createRandomArray;
+
+///  返回数组中的最小值
++ (NSInteger) minValueInArray:(NSArray *)array from:(NSInteger)i;
 
 @end
